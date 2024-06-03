1f0a7a28aa41d2142bf79ac7947b5df13d1c99b5
unknown <enrique.caballero.0602@gmail.com>
2024-06-03 12:04:46 -0500

    avanzando framework para agilizr mas trabajo

--- util/util.php
+++ util/util.php
@@ -7,8 +7,14 @@ class Util{
         return $data;
     }
     public function hash_pass($pass){
+        $pass =trim($pass);
+        $pass=stripslashes($pass);
         $pass=md5($pass);
         return $pass;
+    }
+    public function fecha_hora($date){
+        $date =date('d/m/Y H:i:s');
+        return $date;
     }
 }
 ?>
